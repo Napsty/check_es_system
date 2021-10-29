315755f93d22d9a1a3044d41a7d214a6ed1eec02
Napsty <ck@claudiokuenzler.com>
2021-10-29 13:19:02 +0200

    Test

--- test/test_status.sh
+++ test/test_status.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+echo "Test Elasticsearch status"
 ./check_es_system.sh -H 127.0.0.1 -P 9200 -t status
+
+if [[ $? -eq 0 ]]; then
+  echo -e "\e[1m\e[32m✔ Test 1 OK: Status check worked and shows green\e[0m"
+  exit 0
+else
+  echo -e "\e[1m\e[31m✘ Test 1 ERROR: Status check has not worked\e[0m"
+  exit 1
+fi
