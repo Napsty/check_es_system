52443a01247bf36c8a936a7b5dce267c7a4b2cfb
Napsty <ck@claudiokuenzler.com>
2022-05-13 15:53:16 +0200

    more travis

--- test/test_disk.sh
+++ test/test_disk.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "Test Elasticsearch status"
-sed -i "/#\!\/bin\/bash/ a\set -e" check_es_system.sh
-sed -i '/set -e/ a\trap "echo ERROR: There was an error in ${FUNCNAME-main context}, details to follow" ERR' check_es_system.sh
-head check_es_system.sh
-./check_es_system.sh -H 127.0.0.1 -P 9200 -t disk
+output=$(./check_es_system.sh -H 127.0.0.1 -P 9200 -t disk)
 
 if [[ $? -eq 0 ]]; then
   echo -e "\e[1m\e[32m✔ Test 3.1 OK: Disk check worked and shows green\e[0m"
   exitcode=0
 else
   echo -e "\e[1m\e[31m✘ Test 3.1 ERROR: Disk check has not worked\e[0m"
+  exitcode=1
+fi
+
+if [[ "${output}" != "ES SYSTEM OK - Disk usage is at 0% (0 G from 67 G)|es_disk=648B;58128941056;69028117504;0;72661176320" ]]; then
   exitcode=1
 fi
 
