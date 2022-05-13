47ab903ebe984c024079132c4bed1bbd3ab11951
Napsty <ck@claudiokuenzler.com>
2022-05-13 14:53:12 +0200

    Add disk check to travis ci

---
+++ test/test_disk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+echo "Test Elasticsearch status"
+./check_es_system.sh -H 127.0.0.1 -P 9200 -t disk
+
+if [[ $? -eq 0 ]]; then
+  echo -e "\e[1m\e[32m✔ Test 3.1 OK: Disk check worked and shows green\e[0m"
+  exitcode=0
+else
+  echo -e "\e[1m\e[31m✘ Test 3.1 ERROR: Disk check has not worked\e[0m"
+  exitcode=1
+fi
+
+exit $exitcode
